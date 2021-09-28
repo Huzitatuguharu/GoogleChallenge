e63a5aa87311b9b63b92ffb266353fcd4fbfe79a
Huzitatuguharu <sgrko.4751171@gmail.com>
2021-09-28 16:57:53 +0900

    headercreate

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>
+  <div class="wrapper">
     <Header />
     <Nuxt />
     <Footer />
   </div>
 
 </template>
+
+.<style scoped>
+.wrapper{
+	height: 100%;
+}
+</style>
 <script>
 
 import Header from "@/components/header.vue"; // headerをインポート
